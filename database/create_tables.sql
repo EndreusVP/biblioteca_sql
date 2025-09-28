362e92a06883b59c349ef4c9b7d5b5d047a9c4b5
EndreusSre <endreus.valerio@gmail.com>
2025-09-28 18:50:28 -0300

    corrigindo erros nas tabelas

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -5,28 +5,28 @@ USE biblioteca;
 CREATE TABLE autores (
     IDautor INT PRIMARY KEY AUTO_INCREMENT, 
     nome VARCHAR(30) NOT NULL, 
-    nacionalidade VARCHAR(50), NOT NULL
+    nacionalidade VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE livros (
     IDlivro INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(50) NOT NULL,
     IDautor INT NOT NULL,
-    FOREING KEY (IDautor) REFERENCES autores(IDautor) 
-)
+    FOREIGN KEY (IDautor) REFERENCES autores(IDautor) 
+);
 
-CREATE TABLE usuario (
+CREATE TABLE usuarios (
     IDusuario iNT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     email VARCHAR(50)
-)
+);
 
 CREATE TABLE emprestimos(
-    IDimprestimos INT PRIMARY KEY AUTO_INCREMENT,
+    IDemprestimos INT PRIMARY KEY AUTO_INCREMENT,
     IDusuario INT NOT NULL,
-    FOREING KEY (IDusuario) REFERENCES usuario(IDusuario),
+    FOREIGN KEY (IDusuario) REFERENCES usuario(IDusuario),
     IDlivro INT NOT NULL,
-    FOREING KEY (IDlivro) REFERENCES livros(IDlivros),
+    FOREIGN KEY (IDlivro) REFERENCES livros(IDlivro),
     dataEmprestimo DATE NOT NULL,
     dataDevolucao DATE
-)
+);
