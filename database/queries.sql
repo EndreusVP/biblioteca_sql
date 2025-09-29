095fc0878911861cc1af44be8deee0de86e6dbb2
EndreusSre <endreus.valerio@gmail.com>
2025-09-29 16:58:47 -0300

    inserts

--- database/queries.sql
+++ database/queries.sql
@@ -1,6 +1,24 @@
 USE biblioteca;
 
+/*mostrar livros com nome dos autores*/
 SELECT a.nome, l.titulo
-FROM a AS autores
-JOIN l AS livros
+FROM autores AS a
+JOIN livros AS l
 ON a.IDautor = l.IDautor;
+
+/*mostrar todos os emprestimos de um usuario especifico*/
+SELECT u.nome, l.titulo, e.dataEmprestimo, e.dataDevolucao
+FROM usuarios AS u 
+JOIN emprestimos AS e 
+ON u.IDusuario = e.IDusuario
+JOIN livros AS l
+ON l.IDlivro = e.IDlivro
+WHERE u.nome = "XUXA";
+
+/*livros q nao foram devolvidos*/
+SELECT l.titulo, e.dataDevolucao
+FROM livros AS l
+JOIN emprestimos AS e
+ON l.IDlivro = e.IDlivro
+WHERE e.dataDevolucao IS NULL;
+
